dad6f24479eae067b03dd2cffdf0c0aa8f22e462
ShakebShamsi <shakebshamsi@hotmail.com>
2025-06-19 18:32:39 +0530

    final

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
 	return (
 		<div className="main">
 			<div className="nav">
-				<p>Gemini</p>
+				<p>GenAI Chat</p>
 				<img src={assets.user_icon} alt="" />
 			</div>
 			<div className="main-container">
